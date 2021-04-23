e69af685618acea78e59edfa4f4ad4de5afced36
morganamilo <morganamilo@archlinux.org>
2021-04-23 12:49:59 +0100

    Code cleanup

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,7 @@
-use alpm::vercmp;
-use std::cmp::Ordering;
+use alpm::Version;
 use std::path::Path;
 
 pub fn check_existance(path: &str) -> bool {
     let p = Path::new(path);
     p.exists()
-}
-
-pub fn strvercmp(a: &str, b: &str) -> bool {
-    vercmp(a.as_bytes().to_vec(), b.as_bytes().to_vec()) == Ordering::Greater
-}
-
-#[test]
-fn test_strvercmp() {
-    assert_eq!(strvercmp("0.0.1", "0.0.2"), false);
-    assert_eq!(strvercmp("0.0.1", "0.0.1"), false);
-    assert_eq!(strvercmp("0.0.2", "0.0.1"), true);
 }
